a74467fdf0405bd89cf1e90baa42134184f99743
Juandoqg <juandoqg@gmail.com>
2024-11-15 12:29:18 -0500

    despliegue

--- LineaPurpura/settings.py
+++ LineaPurpura/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k*-nyo!7-^kr&c*jq%8h(!6zmu34y1yhc!j1am+8!4qavuhi_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
