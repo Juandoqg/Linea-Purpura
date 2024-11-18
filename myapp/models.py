255520dbea309971e562591682be2eb569c5c83a
Juandoqg <juandoqg@gmail.com>
2024-11-18 14:27:46 -0500

    imagenes añadidas

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Boton(models.Model):
-    nombre = models.CharField(max_length=50, unique=True)  # Nombre del botón (e.g., boton1)
+    nombre = models.CharField(max_length=50, unique=True)  
     clicks = models.PositiveIntegerField(default=0)# Número de clics
     numero = models.IntegerField(default=0)      
+    imgBoton = models.CharField(max_length=50, default="")
+
 
     def __str__(self):
         return f"{self.nombre} ({self.clicks} clics)"
