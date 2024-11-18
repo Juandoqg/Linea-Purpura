46e45b9ff8362c7075161d8fabe3292807672358
Juandoqg <juandoqg@gmail.com>
2024-11-17 23:50:50 -0500

    cambio de modelo y estilos, faltan img

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Boton(models.Model):
     nombre = models.CharField(max_length=50, unique=True)  # Nombre del botón (e.g., boton1)
-    clicks = models.PositiveIntegerField(default=0)       # Número de clics
+    clicks = models.PositiveIntegerField(default=0)# Número de clics
+    numero = models.IntegerField(default=0)      
 
     def __str__(self):
         return f"{self.nombre} ({self.clicks} clics)"
